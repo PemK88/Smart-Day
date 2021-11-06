4860a06fa0b705a9c12781e3b2f5470c81ee2995
PemK88 <68207162+PemK88@users.noreply.github.com>
2021-11-06 18:26:52 -0400

    Smart Week Route Working

--- smart-day/src/App.js
+++ smart-day/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import Login from "./Login/index.js"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LevelSelect from './LevelSelect/index.js';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-          <Routes> 
+          <Switch> 
             <Route exact path='/' render={() => (<Login/>)}/>
-          </Routes>
-        </BrowserRouter>
+            <Route exact path='/smart' render={() => (<LevelSelect/>)}/>
+          </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ smart-day/src/LevelSelect/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import "./styles.css";
+import PropTypes from 'prop-types';
+import { Container, Row, Col, Button } from "react-bootstrap"
+
+function LevelSelect () {
+
+    return (
+        <div>
+        <h1>hi</h1>
+        <button>hi</button>
+        </div>
+    );
+}
+
+export default LevelSelect;

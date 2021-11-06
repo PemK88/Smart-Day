3931a9190b3217e69c1232d68df4b1a9f231cac4
hamrajk <hamraj_khangura@hotmail.com>
2021-11-06 19:17:35 -0400

    Login Screen

--- smart-day/src/Login/index.js
+++ smart-day/src/Login/index.js
@@ -1,25 +1,38 @@
-import React from 'react'
-import "./Login.css"
-import { Container, Row, Col, Button } from "react-bootstrap"
+import React from "react";
+import "./Login.css";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+
+const Header = () => {
+  return <h1 className="header">Smart Day</h1>;
+};
 
 const Login = () => {
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Col>
-                        <Button className="hello">login</Button>
-                    </Col>
-                </Row>
+  return (
+    <div>
+      <Header />
+      <p className="slogan">Your day is about to get smarter!</p>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            className="username"
+            type="text"
+            placeholder="Username"
+          />
+        </Form.Group>
 
-                <Row>
-                    <Col>
-                        <Button>login</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div >
-    )
-}
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            className="password"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Button className="login-button" variant="primary" type="submit">
+          LOG IN
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;

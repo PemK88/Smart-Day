eeee5eb1aba2ce73177ef387479c42d503b6794f
PemK88 <68207162+PemK88@users.noreply.github.com>
2021-11-06 17:48:27 -0400

    Created Browser Routes

--- smart-day/src/App.js
+++ smart-day/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
-import Login from "./Login.js"
+import Login from "./Login/index.js"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-      </header>
+    <div>
+      <BrowserRouter>
+          <Routes> 
+            <Route exact path='/' render={() => (<Login/>)}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }

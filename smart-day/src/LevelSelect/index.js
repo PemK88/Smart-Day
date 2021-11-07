9db1e6212e658c296aae48e0baa7b996c53b7a27
PemK88 <68207162+PemK88@users.noreply.github.com>
2021-11-06 20:15:11 -0400

    Smart Week Page

--- smart-day/src/LevelSelect/index.js
+++ smart-day/src/LevelSelect/index.js
@@ -1,15 +1,66 @@
 import React, { useState } from 'react';
 import "./styles.css";
-import PropTypes from 'prop-types';
-import { Container, Row, Col, Button } from "react-bootstrap"
+import { DropdownButton, Dropdown } from "react-bootstrap"
 
 function LevelSelect () {
 
+    const [level, setLevel] = useState(new Array(7).fill(''));
+
+    const handleSelect = (idx, selectedValue) => {
+
+        let value = [...level];
+        value[idx] = selectedValue;
+        setLevel(value);
+
+    };
+
+    const generateDropDown = (idx) => {
+        return (
+            <DropdownButton id={idx.toString()} title={level[idx]} className="level-select">
+                <Dropdown.Item href="#/action-1"  onClick={() => {handleSelect(idx,'Chill')}}>Chill</Dropdown.Item>
+                <Dropdown.Item href="#/action-2" onClick={() => {handleSelect(idx,'Study')}}>Study</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" onClick={() => {handleSelect(idx,'Socialize')}}>Socialize</Dropdown.Item>
+            </DropdownButton>
+        );
+    };
+
     return (
-        <div>
-        <h1>hi</h1>
-        <button>hi</button>
+        <div className="page">
+            <h1 className="page-title">My Smart Week</h1>
+
+            <ul className="days-list">
+                <li>
+                    <p>Monday</p>
+                    {generateDropDown(0)} 
+                </li>
+                <li>
+                    <p>Tuesday</p>
+                    {generateDropDown(1)} 
+                </li>
+                <li>
+                    <p>Wednesday</p>
+                    {generateDropDown(2)}
+                </li>
+                <li>
+                    <p>Thursday</p>
+                    {generateDropDown(3)}
+                </li>
+                <li>
+                    <p>Friday</p>
+                    {generateDropDown(4)}
+                </li>
+                <li>
+                    <p>Saturday</p>
+                    {generateDropDown(5)}
+                </li>
+                <li id="no-bottom-border">
+                    <p>Sunday</p>
+                    {generateDropDown(6)}
+                </li>
+            </ul>
         </div>
+        
+        
     );
 }
 
